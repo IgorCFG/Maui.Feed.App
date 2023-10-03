90d1d7af1fd35b62bf4cb300420172bea8b8729d
Igor Castro <35529239+IgorCFG@users.noreply.github.com>
2023-10-03 19:16:54 -0300

    fix json

---
+++ NareiaApp/Infrastructure/Abstractions/IFeedItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NareiaApp.Abstractions.Models
+{
+    public interface IFeedItemModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PhotoUrl { get; set; }
+        public IUserModel User { get; set; }
+    }
+}
---
+++ NareiaApp/Infrastructure/Abstractions/IUserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NareiaApp.Abstractions.Models
+{
+    public interface IUserModel
+    {
+        public string Name { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public bool IsVerified { get; set; }
+    }
+}
